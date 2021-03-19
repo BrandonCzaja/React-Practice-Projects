4fb3cc19089e2878f80e59101f93a6658f87ea3d
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-18 20:16:45 -0500

    Created a dynamic form that logs input data

---
+++ form/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NameForm from "./components/Form.jsx";
+
+function App() {
+	return (
+		<div className="App">
+			<NameForm />
+		</div>
+	);
+}
+
+export default App;
