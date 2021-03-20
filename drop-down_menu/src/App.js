bd3430218b27ba11635d63aa40b70b78975433dd
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-03-19 19:48:36 -0500

    Created a dropdown menu

---
+++ drop-down_menu/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FlavorForm from "./components/DropDownMenu.jsx";
+
+function App() {
+	return (
+		<div className="App">
+			<FlavorForm />
+		</div>
+	);
+}
+
+export default App;
