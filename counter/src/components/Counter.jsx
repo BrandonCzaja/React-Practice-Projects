52e06dec02524fc1dd7668bfa67d32744c98d6e6
Brandon Czaja <Brandon.Czaja@gmail.com>
2021-05-24 16:10:01 -0500

    Created a decrement but as well

--- counter/src/components/Counter.jsx
+++ counter/src/components/Counter.jsx
@@ -6,7 +6,7 @@ class Clock extends React.Component {
 		count: 0,
 	};
 
-	handleClick = (event) => {
+	incrementCounter = (event) => {
 		// This will work without prevState => just remove prevState, and change count: prevState.count to count: count
 		// Check the docs to see if it is preferred to use it or not
 
@@ -15,10 +15,20 @@ class Clock extends React.Component {
 		}));
 	};
 
+	decrementCounter = (event) => {
+		// This will work without prevState => just remove prevState, and change count: prevState.count to count: count
+		// Check the docs to see if it is preferred to use it or not
+
+		this.setState((prevState, { count }) => ({
+			count: prevState.count - 1,
+		}));
+	};
+
 	render() {
 		return (
 			<>
-				<button onClick={this.handleClick}>Click Me!</button>
+				<button onClick={this.incrementCounter}>Increment!</button>
+				<button onClick={this.decrementCounter}>Decrement!</button>
 				<p>{this.state.count}</p>
 			</>
 		);
